4d2ba1048ac56292affd0211c24851b646c45910
X-5Martinez <d505789ha@gmail.com>
2025-10-18 18:21:15 -0600

    Mi primer commit:proyecto inicial

---
+++ app.js
@@ -0,0 +1,7 @@
+function mostrarMensaje() {
+    alert('¡Felicidades! Tu proyecto está funcionando correctamente.');
+    console.log('Proyecto de práctica 2 - GitHub');
+}
+
+// Mensaje en la consola al cargar la página
+console.log('Página cargada exitosamente');
